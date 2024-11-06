f79b2031ba58f1053df0053ddf730433b566c6a1
IGGLOBAL\Arnab.B <arnab.b@infogain.com>
2024-11-06 15:05:08 +0530

    Added base view and initial signals

---
+++ libs/blocks/locui-create/utils/state.js
@@ -0,0 +1,13 @@
+import { signal } from '../../../deps/htm-preact.js';
+
+const emptyProject = {
+  name: '',
+  htmlFlow: false,
+  regionalEditBehavior: '',
+  urls: [],
+  fragments: false,
+  locales: [],
+};
+
+export const currentStep = signal(1);
+export const project = signal({ ...emptyProject });
